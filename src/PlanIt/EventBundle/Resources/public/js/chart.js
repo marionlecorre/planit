5403b18c0e958f61c9ca75c675424b8c29ebdd4d
Camarasa <alizee.camarasa2@gmail.com>
2014-12-24 00:39:26 +0100

    parce que je suis tubé

--- src/PlanIt/EventBundle/Resources/public/js/chart.js
+++ src/PlanIt/EventBundle/Resources/public/js/chart.js
@@ -58,16 +58,6 @@ $(function () {
         });
     });
 
-function getMaxDivHeight(){
-  var maxHeight = 0;
-  $('.event-thumbnail').each(function(i){
-    if(this.offsetHeight > maxHeight)
-      maxHeight = this.offsetHeight;
-  });
-  return maxHeight;
-}
-$('.listing-events').height(getMaxDivHeight());
-$('.add.event-thumbnail').css("line-height",getMaxDivHeight()+"px");
 
 
 });
