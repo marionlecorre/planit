3b51870f80c350c4d0643fdd9af050fccd82f96d
Camarasa <alizee.camarasa2@gmail.com>
2015-01-18 18:52:57 +0100

    accordéon sur la page invités

--- src/PlanIt/GuestsBundle/Resources/public/js/guests.js
+++ src/PlanIt/GuestsBundle/Resources/public/js/guests.js
@@ -5,6 +5,19 @@ function guests(){
 	});
 	$('.sent-true').tooltip();
 	$('.light').tooltip();
+	accordeonGuest();
 }
 
 
+function accordeonGuest(){
+	$('.accordion-body:not(:first)').hide();
+	$('.accordion-head:first').addClass("open");
+	$('.accordion-head').click(function(e){
+		if(e.pageX>325) {
+			$(this).toggleClass("open").next().slideToggle()
+			.siblings('.accordion-body').slideUp();
+			$(this).siblings('.accordion-head').removeClass("open");
+			return false;
+		}
+	});
+}
