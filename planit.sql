ae8922348d86ef444323d7ccbff02e61a63b528c
Camarasa <alizee.camarasa2@gmail.com>
2015-01-18 21:13:34 +0100

    MAJ BDD

--- planit.sql
+++ planit.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost:8889
--- Généré le :  Dim 18 Janvier 2015 à 19:39
+-- Généré le :  Dim 18 Janvier 2015 à 21:12
 -- Version du serveur :  5.5.38
 -- Version de PHP :  5.6.2
 
@@ -19,6 +19,109 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `planit`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `event`
+--
+
+CREATE TABLE `event` (
+`id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `name` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `slug` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `description` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `begin_date` datetime NOT NULL,
+  `end_date` datetime NOT NULL,
+  `image` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `event`
+--
+
+INSERT INTO `event` (`id`, `user_id`, `name`, `slug`, `description`, `begin_date`, `end_date`, `image`) VALUES
+(1, 1, 'Nouvel An', 'nouvelan', 'On va boire du champagne faire la fête et tout et tout', '2014-12-31 00:00:00', '2015-01-01 00:00:00', 'nouvel-an.jpg'),
+(2, 2, 'Noël', 'noel', 'Petit papa noel quand tu descendras du ciel... tu m''amèneras un macbook Pro stp', '2014-12-24 00:00:00', '2014-12-25 00:00:00', 'noel.jpg'),
+(3, 3, 'Anniversaire Florent', 'annivflorent', 'Hapyy birthday to youuu Diabeto <3 get on the flow', '2014-11-19 00:00:00', '2014-11-20 00:00:00', 'photo_evenement.jpg'),
+(4, 3, 'Mariage d''Aurélie', 'mariageaurelie', 'Beau mariage à Lavaur avec Marina en demoiselle d''honneur en robe rose', '2015-04-16 00:00:00', '2015-04-17 00:00:00', 'mariage.jpg'),
+(5, 3, 'Gala', 'Gala', 'Soirée de ouf sur une péniche', '2015-01-23 20:00:00', '2015-01-24 05:00:00', '3-5245.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `guest`
+--
+
+CREATE TABLE `guest` (
+`id` int(11) NOT NULL,
+  `type_guest_id` int(11) DEFAULT NULL,
+  `module_id` int(11) DEFAULT NULL,
+  `firstname` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `lastname` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `confirmed` int(11) NOT NULL,
+  `payed` int(11) NOT NULL,
+  `sent` tinyint(1) NOT NULL,
+  `paymentmean_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `guest`
+--
+
+INSERT INTO `guest` (`id`, `type_guest_id`, `module_id`, `firstname`, `lastname`, `email`, `confirmed`, `payed`, `sent`, `paymentmean_id`) VALUES
+(1, 1, 4, 'Alizée', 'Camarasa', 'babouche@kira.com', 1, 1, 1, NULL),
+(2, 1, 4, 'Marion', 'Lecorredula', 'lamoche@jackson.com', 2, 0, 0, NULL),
+(3, 1, 4, 'Marina', 'Avataneo', 'warina@grisou.com', 1, 0, 0, NULL),
+(4, 2, 4, 'Bill', 'Boquet', 'bill@pasdechat.com', 1, 1, 0, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `migration_versions`
+--
+
+CREATE TABLE `migration_versions` (
+  `version` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `module`
+--
+
+CREATE TABLE `module` (
+`id` int(11) NOT NULL,
+  `event_id` int(11) DEFAULT NULL,
+  `name` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `slug` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `int_type` int(11) NOT NULL,
+  `type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `list_type` int(11) DEFAULT NULL,
+  `max_guests` int(11) DEFAULT NULL,
+  `payable` tinyint(1) DEFAULT NULL,
+  `max_budget` int(11) DEFAULT NULL,
+  `max_capacity_p` int(11) DEFAULT NULL,
+  `max_price_p` int(11) DEFAULT NULL,
+  `max_time_to_go` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `max_capacity_t` int(11) DEFAULT NULL,
+  `max_price_t` int(11) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `module`
+--
+
+INSERT INTO `module` (`id`, `event_id`, `name`, `slug`, `int_type`, `type`, `list_type`, `max_guests`, `payable`, `max_budget`, `max_capacity_p`, `max_price_p`, `max_time_to_go`, `max_capacity_t`, `max_price_t`) VALUES
+(2, 1, 'Invités', 'guests', 1, 'guests', NULL, 200, 1, NULL, NULL, NULL, NULL, NULL, NULL),
+(3, 2, 'TODO', 'todo', 2, 'todo', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(4, 3, 'Invités', 'guests', 1, 'guests', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(5, 4, 'invités', 'guests', 1, 'guests', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(6, 2, 'Invitations', 'invitations', 1, 'guests', 1, 300, 1, NULL, NULL, NULL, NULL, NULL, NULL),
+(7, 5, 'Invitations', 'invitations', 1, 'guests', 1, 200, 1, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -41,9 +144,92 @@ INSERT INTO `PaymentMeans` (`id`, `label`) VALUES
 (3, 'Espèce'),
 (4, 'Virement');
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `paymentmeans_module`
+--
+
+CREATE TABLE `paymentmeans_module` (
+  `guestsmodule_id` int(11) NOT NULL,
+  `paymentmeans_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `TypeGuest`
+--
+
+CREATE TABLE `TypeGuest` (
+`id` int(11) NOT NULL,
+  `module_id` int(11) DEFAULT NULL,
+  `label` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `message` tinytext COLLATE utf8_unicode_ci NOT NULL,
+  `price` double NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `TypeGuest`
+--
+
+INSERT INTO `TypeGuest` (`id`, `module_id`, `label`, `message`, `price`) VALUES
+(1, 4, 'Amis IMAC', 'coucou mes amis imac', 20),
+(2, 4, 'Amis SRC', 'coucou mes amis src', 30),
+(3, 4, 'Famille', 'Coucou', 70),
+(4, 7, 'Imac adhérents', 'Bonjour viens au gala', 26);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+`id` int(11) NOT NULL,
+  `name` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `surname` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `mail` tinytext COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+  `image` varchar(30) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `surname`, `mail`, `password`, `image`) VALUES
+(1, 'Babouche', 'Ali', 'alizee.camarasa@gmail.com', '28012904', 'kira.jpg'),
+(2, 'LeCorre', 'Marion', 'marion@lecorre.com', '0000', 'jackson.jpg'),
+(3, 'Avataneo', 'Warina', 'marina@avtaneo.com', '0000', 'grisou.jpg');
+
 --
 -- Index pour les tables exportées
 --
+
+--
+-- Index pour la table `event`
+--
+ALTER TABLE `event`
+ ADD PRIMARY KEY (`id`), ADD KEY `IDX_3BAE0AA7A76ED395` (`user_id`);
+
+--
+-- Index pour la table `guest`
+--
+ALTER TABLE `guest`
+ ADD PRIMARY KEY (`id`), ADD KEY `IDX_6D76B53157B581A4` (`type_guest_id`), ADD KEY `IDX_6D76B531AFC2B591` (`module_id`), ADD KEY `IDX_6D76B5311594D248` (`paymentmean_id`);
+
+--
+-- Index pour la table `migration_versions`
+--
+ALTER TABLE `migration_versions`
+ ADD PRIMARY KEY (`version`);
+
+--
+-- Index pour la table `module`
+--
+ALTER TABLE `module`
+ ADD PRIMARY KEY (`id`), ADD KEY `IDX_C24262871F7E88B` (`event_id`);
 
 --
 -- Index pour la table `PaymentMeans`
@@ -52,14 +238,94 @@ ALTER TABLE `PaymentMeans`
  ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `paymentmeans_module`
+--
+ALTER TABLE `paymentmeans_module`
+ ADD PRIMARY KEY (`guestsmodule_id`,`paymentmeans_id`), ADD KEY `IDX_A4BA4E093A6610AA` (`guestsmodule_id`), ADD KEY `IDX_A4BA4E099F63E055` (`paymentmeans_id`);
+
+--
+-- Index pour la table `TypeGuest`
+--
+ALTER TABLE `TypeGuest`
+ ADD PRIMARY KEY (`id`), ADD KEY `IDX_65AD8281AFC2B591` (`module_id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+ ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT pour les tables exportées
 --
 
+--
+-- AUTO_INCREMENT pour la table `event`
+--
+ALTER TABLE `event`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT pour la table `guest`
+--
+ALTER TABLE `guest`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT pour la table `module`
+--
+ALTER TABLE `module`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT pour la table `PaymentMeans`
 --
 ALTER TABLE `PaymentMeans`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT pour la table `TypeGuest`
+--
+ALTER TABLE `TypeGuest`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `event`
+--
+ALTER TABLE `event`
+ADD CONSTRAINT `FK_3BAE0AA7A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Contraintes pour la table `guest`
+--
+ALTER TABLE `guest`
+ADD CONSTRAINT `FK_6D76B5311594D248` FOREIGN KEY (`paymentmean_id`) REFERENCES `PaymentMeans` (`id`),
+ADD CONSTRAINT `FK_6D76B53157B581A4` FOREIGN KEY (`type_guest_id`) REFERENCES `TypeGuest` (`id`),
+ADD CONSTRAINT `FK_6D76B531AFC2B591` FOREIGN KEY (`module_id`) REFERENCES `module` (`id`);
+
+--
+-- Contraintes pour la table `module`
+--
+ALTER TABLE `module`
+ADD CONSTRAINT `FK_C24262871F7E88B` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`);
+
+--
+-- Contraintes pour la table `paymentmeans_module`
+--
+ALTER TABLE `paymentmeans_module`
+ADD CONSTRAINT `FK_A4BA4E099F63E055` FOREIGN KEY (`paymentmeans_id`) REFERENCES `PaymentMeans` (`id`) ON DELETE CASCADE,
+ADD CONSTRAINT `FK_A4BA4E093A6610AA` FOREIGN KEY (`guestsmodule_id`) REFERENCES `module` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `TypeGuest`
+--
+ALTER TABLE `TypeGuest`
+ADD CONSTRAINT `FK_65AD8281AFC2B591` FOREIGN KEY (`module_id`) REFERENCES `module` (`id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
