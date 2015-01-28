1679a9e033b44a54663ee9e47556262c965f73a3
Camarasa <alizee.camarasa2@gmail.com>
2015-01-29 00:22:07 +0100

    motif pépin ajout module guest

--- src/PlanIt/GuestsBundle/Form/GuestsModuleType.php
+++ src/PlanIt/GuestsBundle/Form/GuestsModuleType.php
@@ -17,7 +17,7 @@ class GuestsModuleType extends ModuleType
     {
         parent::buildForm($builder, $options);
         $builder
-            ->add('list_type', 'text', array('label'  => false, 'attr' => array('placeholder' => 'Invitation ou inscription?')))
+            ->add('guestmodule_type', 'choice', array('label'  => false,'choices'  => array('0' => 'Invitation', '1' => 'Inscription')))
             ->add('max_guests' , 'text', array('label'  => false, 'attr' => array('placeholder' => 'Nombre d\'invité maximum')))
             ->add('payable', 'checkbox', array('label'  => 'Événement payant?'))
         ;
